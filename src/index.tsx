d7b15ca3ea170f3a4fdc883307a5d70ac32facc7
Arnastria <arnas.bgr@gmail.com>
2021-09-06 20:14:29 +0700

    fix euuy

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { Persistor, reduxStore } from './redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={reduxStore}>
+      <PersistGate
+        loading={<>Loading..</>}
+        persistor={Persistor}
+      >
+        <App />
+      </PersistGate>
+
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
